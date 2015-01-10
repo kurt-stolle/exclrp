5cab72130af03a9995cf9bd9cae46caf6981cdaa
Kurt Stolle <kurt@casualbananas.com>
2015-01-10 18:28:50 +0100

    Update 2.5
    Small visual tweak to action menu

--- gamemode/core/cl_actionmenu.lua
+++ gamemode/core/cl_actionmenu.lua
@@ -14,14 +14,14 @@ hook.Add("PrePaintMainHUD","exclDrawActionMenus",function()
 	if actionmenu then
 		
 		local scrPos = actionmenuPos:ToScreen();
-		draw.RoundedBox(4,scrPos.x-51,scrPos.y-1,102,#actionmenuOptions*20 + 2,Color(0,0,0))
+		draw.RoundedBox(2,scrPos.x-51,scrPos.y-1,102,#actionmenuOptions*20 + 2,Color(0,0,0))
 		for k,v in pairs(actionmenuOptions)do
 			if ScrW()/2 < scrPos.x-50+100 and ScrW()/2 > scrPos.x-50 and ScrH()/2 < scrPos.y+ 20*(k-1)+20 and ScrH()/2 >  scrPos.y+ 20*(k-1) then
 				v.hover =true;
-				draw.RoundedBox(4,scrPos.x-50,scrPos.y+ 20*(k-1),100,20,Color(0,0,100+math.sin(CurTime())*50));
+				draw.RoundedBox(2,scrPos.x-50,scrPos.y+ 20*(k-1),100,20,Color(0,0,100+math.sin(CurTime())*50));
 			else
 				v.hover = false;
-				draw.RoundedBox(4,scrPos.x-50,scrPos.y+ 20*(k-1),100,20,Color(0,0,0));
+				draw.RoundedBox(2,scrPos.x-50,scrPos.y+ 20*(k-1),100,20,Color(0,0,0));
 			end
 			
 			draw.RoundedBox(2,scrPos.x-49,scrPos.y+ 20*(k-1) + 1,98,9,Color(255,255,255,10));
